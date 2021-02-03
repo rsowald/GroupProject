7e2c9c31b0dbce33c3e8a7bdc4e776a560ab84d1
rnick1 <rnick0260@gmail.com>
2021-02-02 21:25:16 -0800

    Added initial js content.

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //JavaScript
+var issueContainer = document.getElementById('issues');
+var fetchButton = document.getElementById('fetch-button');
+
+function getRecipePuppy() {
+  var requestUrl = 'http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3';
+//   i=ingredients, remember to separate with a comma and q=recipe title p=page (maybe we can do without this?)
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+            var title = document.createElement('h1')
+            var link = document.createElement('p')
+            // forEach/forloop
+            var ingredients = document.createElement('ul')
+            var ingredient = document.createElement('li')
+
+            title.textContent = data.results[i].title;
+            link.textContent = data.results[i].href;
+            ingredient.textContent = data.results[i].ingredients
+
+     
+     
+     
+     
+     
+
+
+
+        var userName = document.createElement('h3');
+        var issueTitle = document.createElement('h4');
+        var issueBody = document.createElement('p');
+
+        issueContainer.append(userName);
+        issueContainer.append(issueTitle);
+        issueContainer.append(issueBody);
+      }
+    });
+}
+fetchButton.addEventListener('click', getApi);
