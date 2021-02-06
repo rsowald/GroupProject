bcad101b954acf4f8b91a1b48450dc74f25770b1
rsowald <72467920+rsowald@users.noreply.github.com>
2021-02-06 11:48:07 -0800

    trying to fix API calls

--- script.js
+++ script.js
@@ -1,66 +1,48 @@
 var recipesresults = document.querySelector('#recipesResults');
-var userSearch = document.querySelector('#user-Search');
+// var userSearch = document.querySelector('#user-search');
+var searchBtn = document.querySelector('#search-btn');
+var randomBtn = document.querySelector('#random-btn');
 
-function getRecipePuppy() {
-  var requestPuppy = 'http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3';
-//   i=ingredients, remember to separate with a comma and q=recipe title p=page (maybe we can do without this?)
+var userSearch = "s=" + $("#user-search").val()
 
-  fetch(requestPuppy)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            var title = document.createElement('h2')
-            var link = document.createElement('p')
-            // for loop or forEach
-            var ingredientList = document.createElement('ul')
-            var ingredient = document.createElement('li')
-            title.textContent = data.results[i].title;
-            link.textContent = data.results[i].href;
-            ingredient.textContent = data.results[i].ingredients
-            recipesresults.append(title);
-            recipesresults.append(link);
-            recipesresults.append(ingredient);
-            }
-    });
+function getRecipeByName(userSearch) {
+  var requestRecipeByName = 'https://www.themealdb.com/api/json/v1/1/search.php?';
 
+  console.log(userSearch)
+  $.get(requestRecipeByName, userSearch)
+    .done(console.log(data))
+    .fail(console.log("error"));
 
-
-
-
-
-
-
-
-
-
-    function getMealIDB() {
-      var requestMealIDB = 'https://www.themealdb.com/api/json/v1/1/random.php';
-
-    fetch(requestMealIDB)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            // var title = document.createElement('h1')
-            // var link = document.createElement('p')
-            // var ingredientList = document.createElement('ul')
-            // var ingredient = document.createElement('li')
-
-            // title.textContent = data.results[i].title;
-            // link.textContent = data.results[i].href;
-            // ingredient.textContent = data.results[i].ingredients
-
-            // issueContainer.append(title);
-            // issueContainer.append(link);
-            // issueContainer.append(ingredient);
-            }
-    });
-  }
 }
-fetchButton.addEventListener('click', getMealIDB);
-fetchButton.addEventListener('click', getApi);
+
+
+
+
+function getMealDB() {
+  var requestMealDB = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+  fetch(requestMealDB)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        // var title = document.createElement('h1')
+        // var link = document.createElement('p')
+        // var ingredientList = document.createElement('ul')
+        // var ingredient = document.createElement('li')
+
+        // title.textContent = data.results[i].title;
+        // link.textContent = data.results[i].href;
+        // ingredient.textContent = data.results[i].ingredients
+
+        // issueContainer.append(title);
+        // issueContainer.append(link);
+        // issueContainer.append(ingredient);
+      }
+    });
+}
+
+randomBtn.addEventListener('click', getMealDB);
+searchBtn.addEventListener('click', getRecipeByName);
