d1707390b16363b65016bf4c910e922d7ffd00e8
rnick1 <rnick0260@gmail.com>
2021-02-08 18:26:49 -0800

    SImplified html.

--- script.js
+++ script.js
@@ -78,13 +78,14 @@ $(document).ready(function () {
     });
   }
 
-  // $('.modal-content').on('click', 'button', function (event) {
-  //   event.preventDefault();
-  //   var text = $(this).children('textarea').val();
-  //   var userInput = text;
-  //   var id = $(this).attr('id');
-  //   localStorage.setItem(id, userInput);
-  // })
+  $('#modal-main-content').on('click', 'a', function (event) {
+    event.preventDefault();
+    var text = $(this).children('textarea').val();
+    var userInput = text;
+    console.log(userInput);
+    var id = $(this).attr('id');
+    localStorage.setItem(id, userInput);
+  })
 
   $('.modal').modal();
 
